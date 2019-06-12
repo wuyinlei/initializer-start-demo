de94f00e5153785c19c553b91ddd833eed78fb09
wuyinlei <power1118@gmail.com>
2019-06-12 10:15:04 +0800

    Initial commit

---
+++ src/main/java/com/spring/cloud/initializerstartdemo/InitializerStartDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.initializerstartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerStartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerStartDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/cloud/initializerstartdemo/InitializerStartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.initializerstartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializerStartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
